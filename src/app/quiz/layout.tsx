e53863b7185e8885c3be2a09e65c24fbfb8790d2
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2023-12-27 23:31:51 +0530

    quiz api fetching done

---
+++ src/app/quiz/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <body>{children}</body>
+  );
+}
+
+export default Layout;

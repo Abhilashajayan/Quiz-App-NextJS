ae24f53d03cfdc72b1d1811e5a13edd4cd400db4
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2023-12-28 17:06:43 +0530

    the answer checking error fixing

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -25,83 +25,93 @@ interface Question {
 }
 
 
+
 const page = () => {
-    const [questions, setQuestions] = useState<Question[]>([]);
-    const [selectedValues, setSelectedValues] = useState<string[]>([]);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
 
-    useEffect(() => {
-      const fetchQuestions = async () => {
-        try {
-          const response = await axios.get('https://quizapi.io/api/v1/questions', {
-            params: {
-              apiKey: 'WQnUx8t0JuGPsaEja3g9qDFASaP1wAE9akzfbIIY',
-              limit: 30,
-            },
-          });
-          setQuestions(response.data);
-          console.log(response.data);
-        } catch (error) {
-          console.error('Error fetching questions:', error);
-        }
-      };
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await axios.get('https://quizapi.io/api/v1/questions', {
+          params: {
+            apiKey: 'WQnUx8t0JuGPsaEja3g9qDFASaP1wAE9akzfbIIY',
+            limit: 30,
+          },
+        });
+        setQuestions(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching questions:', error);
+      }
+    };
+
+    fetchQuestions();
+  }, []);
+
+  const handleSelect = (index: number, selectedValue: string) => {
+    const newSelectedValues = [...selectedValues];
+    newSelectedValues[index] = `${selectedValue}_correct`;
+    setSelectedValues(newSelectedValues);
+    console.log(newSelectedValues);
+  };
+  const handleSubmit = () => {
+    const result = questions.map((question, index) => {
+      console.log(question.correct_answers);
+    
+    });
+     
+  };
   
-      fetchQuestions();
-    }, []);
-
-    const handleSelect = (index: number, selectedValue: string) => {
-        const newSelectedValues = [...selectedValues];
-        newSelectedValues[index] = selectedValue;
-        setSelectedValues(newSelectedValues);
-        console.log(selectedValue);
-      };
-      const handleSubmit = () => {
-        console.log('Selected Values:', selectedValues);
-       
-      
-      };
-      
-      
-      
-      
-      
-      
-      
-
+  
+  
   return (
     <>
-    <h2 className="text-2xl font-bold mb-4 text-center text-blue-600">Quiz Questions</h2>
-    <div className="flex  justify-center">
-      <ul>
-        {questions.map((question, index) => (
-          <li key={question.id} className="mb-6">
-            <p className="text-xl font-semibold mb-2">{index + 1}.{question.question}</p>
-            <ul className="p-2 list-disc">
-              {Object.entries(question.answers).map(([key, value]) => (
-                <li key={key}>
-                  <label className="flex items-center">
-                    <input
-                      type="radio"
-                      name={`question_${index}`}
-                      className="mr-2"
-                      value={value}
-                      checked={selectedValues[index] === value}
-                      onChange={() => handleSelect(index, value)}
-                    />
-                    <span className="text-blue-600">{value}</span>
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-        <button onClick={handleSubmit} type="button" className="w-full text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Submit</button>
-      </ul>
-     
-    </div>
-   
-  </>
-  
-  )
-}
+      <h2 className="text-2xl font-bold mb-4 text-center text-blue-600">Quiz Questions</h2>
+      <div className="flex justify-center">
+        <ul>
+          {questions.map((question, index) => (
+            <li key={question.id} className="mb-6">
+              <p className="text-xl font-semibold mb-2">{index + 1}.{question.question}</p>
+              <ul className="p-2 list-disc">
+                {Object.entries(question.answers).map(([key, value]) => (
+                  <li key={key}>
+                    <label className={`flex items-center ${selectedValues[index] === `Option ${key}` ? 'selected' : ''}`}>
+                      <input
+                        type="radio"
+                        name={`question_${index}`}
+                        className="mr-2"
+                        value={key}
+                        checked={selectedValues[index] === `Option ${key}`}
+                        onChange={() => handleSelect(index, key)}
+                      />
+                      <span className="text-blue-600">{value}</span>
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+          <button
+            onClick={handleSubmit}
+            type="button"
+            className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
+          >
+            Submit
+          </button>
+        </ul>
+      </div>
+      <style jsx>{`
+          .selected {
+            background-color: #a0aec0;
+          }
+        `}</style>
+    </>
+  );
+};
 
-export default page
+export default page;
+
+
+
+
